c4b90e25eb950a881b2f5e3c40f2ab7f359c34b8
Chetan Rajauria <chetanrajauria@gmail.com>
2025-03-11 23:22:52 +0530

    Initial Commit

---
+++ SwiftUIListView/SwiftUIListViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListViewApp.swift
+//  SwiftUIListView
+//
+//  Created by Ruchi Kumar on 11/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
